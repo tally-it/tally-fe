cc0669b535b1b8a8f2e2668c514633b003ff01db
Markus Sommer <beardcoder@posteo.de>
2018-02-02 23:28:54 +0100

    TASK: Add incrementItem

--- src/components/Person.js
+++ src/components/Person.js
@@ -7,7 +7,9 @@ class Person extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal: false
+            modal: false,
+            name: props.characteristics.name,
+            amount: props.characteristics.amount
         };
 
         this.toggle = this.toggle.bind(this);
@@ -19,39 +21,55 @@ class Person extends Component {
         });
     }
 
-    render() {
-        const characteristics = this.props.characteristics;
-        return (
-            <Col className={'mb-4'} lg={3} md={4} sm={6}>
+    IncrementItem = () => {
+        this.setState({
+            amount: (parseFloat(this.state.amount) + 1.00)
+        });
+    };
 
-                <Card inverse={true} className={'person'}>
+    IncrementItem2 = () => {
+        this.setState({
+            amount: (parseFloat(this.state.amount) + 2.00)
+        });
+    };
+
+    IncrementItem25 = () => {
+        this.setState({
+            amount: (parseFloat(this.state.amount) + 2.50)
+        });
+    };
+
+    render() {
+        return <Col className={'mb-4'} lg={3} md={4} sm={6}>
+            <Card inverse={true} className={'person'}>
                 <button type={'button'} onClick={this.toggle}>
                     <CardBody>
-                        <CardTitle>{characteristics.name}</CardTitle>
+                        <CardTitle>{this.state.name}</CardTitle>
                         <CardText>
                             <Currency
-                                quantity={characteristics.amount}
+                                quantity={this.state.amount}
                                 currency="EUR"
                             />
                         </CardText>
                     </CardBody>
                 </button>
-                </Card>
-                <Modal tabindex="-1" role="dialog" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>{characteristics.name}</ModalHeader>
-                    <ModalBody>
-                        Aktueller Kontostand: <Currency
-                        quantity={characteristics.amount}
-                        currency="EUR"
-                    />
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
-            </Col>
-        );
+            </Card>
+            <Modal tabindex="-1" role="dialog" isOpen={this.state.modal} toggle={this.toggle}
+                   className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>{this.state.name}</ModalHeader>
+                <ModalBody>
+                    Aktueller Kontostand: <Currency
+                    quantity={this.state.amount}
+                    currency="EUR"
+                />
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={this.IncrementItem}>+ 1.0 €</Button>{' '}
+                    <Button color="secondary" onClick={this.IncrementItem2}>+ 2.00 €</Button>
+                    <Button color="secondary" onClick={this.IncrementItem25}>+ 2.50 €</Button>
+                </ModalFooter>
+            </Modal>
+        </Col>;
     }
 }
 
