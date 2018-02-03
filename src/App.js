fbdb2ae9cea5d3f6bf8555a61d5afdb69ad949f1
Markus Sommer <beardcoder@posteo.de>
2018-02-03 22:07:47 +0100

    TASK: Style navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, {Component} from 'react';
-import './App.scss';
-
-import {
-    Container,
-    Row,
-    Col,
-    Nav,
-    NavItem,
-} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Persons from './containers/Persons';
 import About from './containers/About';
-import {
-    BrowserRouter as Router,
-    Route,
-    NavLink
-} from 'react-router-dom'
+import './App.scss';
+import Navigatin from './components/Navigation';
 
 class App extends Component {
     render() {
@@ -22,13 +12,8 @@ class App extends Component {
             <Container className='h-100' fluid>
                 <Router>
                     <Row className='justify-content-center h-100'>
-                        <Col className='col-auto bg-dark pt-5 nav-pills'>
-                            <Nav vertical={true}>
-                                <NavItem><NavLink className='nav-link text-light' activeClassName="active" exact={true} to="/">Home</NavLink></NavItem>
-                                <NavItem><NavLink className='nav-link text-light' activeClassName="active" exact={true} to="/about">About</NavLink></NavItem>
-                                <NavItem><NavLink className='nav-link text-light' activeClassName="active" exact={true} to="/persons">Persons</NavLink></NavItem>
-                            </Nav>
-
+                        <Col className='col-auto no-padding bg-dark pt-5'>
+                            <Navigatin />
                         </Col>
                         <Col className='pt-5 is-scoll'>
                             <Route path="/about" component={About}/>
